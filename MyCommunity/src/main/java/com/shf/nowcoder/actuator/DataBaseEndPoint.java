01df86e1b3c474812f9876df891c18a0b126de07
shuhongfan <s@shfs.cf>
2022-12-23 18:09:28 +0800

    第八章end

---
+++ MyCommunity/src/main/java/com/shf/nowcoder/actuator/DataBaseEndPoint.java
@@ -0,0 +1,33 @@
+package com.shf.nowcoder.actuator;
+
+import com.shf.nowcoder.util.CommunityUtil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Component
+@Endpoint(id="database")
+public class DataBaseEndPoint {
+    private static final Logger logger = LoggerFactory.getLogger(DataBaseEndPoint.class);
+
+    @Autowired
+    private DataSource dataSource;
+
+    @ReadOperation
+    public String checkConnection() {
+        try {
+            Connection conn = dataSource.getConnection();
+        } catch (SQLException e) {
+            logger.error("获取连接失败："+e.getMessage());
+            return CommunityUtil.getJSONString(1, "获取连接失败");
+        }
+    }
+}
